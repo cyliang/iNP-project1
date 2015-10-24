50559b1540f4d9708e3c584f348093b7b015e94e
Chih-Yung Liang <stu89424@gmail.com>
2015-10-25 05:26:31 +0800

    Make function "prompt" and print welcome message.

--- shell.cpp
+++ shell.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 using namespace std;
 
+inline
+bool prompt(string &line) {
+	cout << "% ";
+	getline(cin, line);
+	return cin;
+}
+
 int main() {
-	string inp;
-	while(cin >> inp) {
-		cout << inp << endl;
+	chdir(RAS_ROOT);
+	cout
+		<< "****************************************\n"
+		<< "** Welcome to the information server. **\n"
+		<< "****************************************" << endl;
+
+	string input_line;
+	while (prompt(input_line)) {
+		cout << input_line << endl;
 	}
 }
