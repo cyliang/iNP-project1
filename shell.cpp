a6611049e44c8eb7ee4530d0657bf833682b218b
Chih-Yung Liang <stu89424@gmail.com>
2015-10-25 18:58:57 +0800

    Process special commands.
    exit, setenv, and printenv.

--- shell.cpp
+++ shell.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <map>
+#include <cstdlib>
 #include <unistd.h>
 using namespace std;
 
@@ -10,8 +13,14 @@ bool prompt(string &line) {
 	return cin;
 }
 
+bool special_cmd(istream &cmd);
+void parse_cmd(istream &cmd);
+
+map<string, string> env_variables;
+
 int main() {
 	chdir(RAS_ROOT);
+	env_variables["PATH"] = "bin:.";
 	cout
 		<< "****************************************\n"
 		<< "** Welcome to the information server. **\n"
@@ -19,6 +28,36 @@ int main() {
 
 	string input_line;
 	while (prompt(input_line)) {
-		cout << input_line << endl;
+		istringstream iss(input_line);
+
+		if (!special_cmd(iss))
+			parse_cmd(iss);
 	}
+}
+
+bool special_cmd(istream &cmd) {
+	string arg;
+
+	cmd >> arg;
+	if (arg == "exit") {
+		exit(0);
+	} else if (arg == "printenv") {
+		cmd >> arg;
+		if (env_variables.count(arg))
+			cout << arg << "=" << env_variables[arg] << endl;
+	} else if (arg == "setenv") {
+		string arg2;
+
+		cmd >> arg >> arg2;
+		env_variables[arg] = arg2;
+	} else {
+		return false;
+	}
+
+	return true;
+}
+
+void parse_cmd(istream &cmd) {
+	string arg;
+	cout << "Not parsed yet!" << endl;
 }
